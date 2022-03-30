8d7084b9bbb3bd7adc578966e83fb4f5c5bc6d09
OmarElhusseini <95829813+OmarElhusseini@users.noreply.github.com>
2022-03-31 00:15:51 +0200

    Add files via upload

---
+++ 3-Micro services/microservices/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dog (id, breed, name, origin) VALUES (1, 'AN','Weza 1', 'africa 1');
+INSERT INTO dog (id,  breed, name, origin) VALUES (2, 'BN','Weza 2', 'africa 2');
+INSERT INTO dog (id,  breed, name, origin) VALUES (3, 'CN','Weza 3', 'africa 3');
+INSERT INTO dog (id,  breed, name, origin) VALUES (4, 'DN','Weza 4', 'africa 4');
+INSERT INTO dog (id,  breed, name, origin) VALUES (5, 'EN','Weza 5', 'africa 5');
+INSERT INTO dog (id,  breed, name, origin) VALUES (6, 'FN','Weza 6', 'africa 6');
